de563bd9917b7c7a6b72cf1f7e86b0f071337e3d
kirato77 <justkirato@gmail.com>
2023-10-26 15:03:34 +0200

    update: name, icon and size

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ import pygame
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((1920, 1080))
+icon = pygame.image.load("./assets/icon.png")
+pygame.display.set_caption("Jeu Loufoque")
+pygame.display.set_icon(icon)
 clock = pygame.time.Clock()
 running = True
 dt = 0
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+speed = 600
 
 while running:
     # poll for events
@@ -20,17 +24,17 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
+    pygame.draw.circle(screen, "red", player_pos, 30)
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_z]:
-        player_pos.y -= 300 * dt
+        player_pos.y -= speed * dt
     if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
+        player_pos.y += speed * dt
     if keys[pygame.K_q]:
-        player_pos.x -= 300 * dt
+        player_pos.x -= speed * dt
     if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+        player_pos.x += speed * dt
 
     # flip() the display to put your work on screen
     pygame.display.flip()
