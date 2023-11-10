7cde8ee0bca1578d248a56a08b7620b3cf107bee
4lex42 <a.laroy@students.ephec.be>
2023-11-10 10:28:55 +0100

    add 2 players and turns-based

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ fenetre = pygame.display.set_mode((largeur, hauteur))
 # Définir les couleurs
 BLANC = (255, 255, 255)
 NOIR = (0, 0, 0)
-ROSE = (255, 105, 180)
 
 # Définir la taille de la case et la marge
 TAILLE_CASE = 100
@@ -40,29 +39,33 @@ cavalier = pygame.image.load("assets/cavalier.png")
 cavalier = pygame.transform.scale(cavalier, (80, 80))
 
 # Créer une liste pour les positions et types de pions
-pions = pions = [
-    {"image": roi, "position": (0, 4)},
-    {"image": reine, "position": (0, 3)},
-    {"image": fou, "position": (0, 2)},
-    {"image": fou, "position": (0, 5)},
-    {"image": cavalier, "position": (0, 1)},
-    {"image": cavalier, "position": (0, 6)},
-    {"image": tour, "position": (0, 0)},
-    {"image": tour, "position": (0, 7)}
-    ] + [
-    {"image": pion, "position": (1, i)} for i in range(8)
+pions = [
+    {"image": roi, "position": (0, 4), "joueur": 1},
+    {"image": reine, "position": (0, 3), "joueur": 1},
+    {"image": fou, "position": (0, 2), "joueur": 1},
+    {"image": fou, "position": (0, 5), "joueur": 1},
+    {"image": cavalier, "position": (0, 1), "joueur": 1},
+    {"image": cavalier, "position": (0, 6), "joueur": 1},
+    {"image": tour, "position": (0, 0), "joueur": 1},
+    {"image": tour, "position": (0, 7), "joueur": 1}
 ] + [
-    {"image": tour, "position": (7, 0)},
-    {"image": cavalier, "position": (7, 1)},
-    {"image": fou, "position": (7, 2)},
-    {"image": reine, "position": (7, 3)},
-    {"image": roi, "position": (7, 4)},
-    {"image": fou, "position": (7, 5)},
-    {"image": cavalier, "position": (7, 6)},
-    {"image": tour, "position": (7, 7)}
+    {"image": pion, "position": (1, i), "joueur": 1} for i in range(8)
 ] + [
-    {"image": pion, "position": (6, i)} for i in range(8)
+    {"image": tour, "position": (7, 0), "joueur": 2},
+    {"image": cavalier, "position": (7, 1), "joueur": 2},
+    {"image": fou, "position": (7, 2), "joueur": 2},
+    {"image": reine, "position": (7, 3), "joueur": 2},
+    {"image": roi, "position": (7, 4), "joueur": 2},
+    {"image": fou, "position": (7, 5), "joueur": 2},
+    {"image": cavalier, "position": (7, 6), "joueur": 2},
+    {"image": tour, "position": (7, 7), "joueur": 2}
+] + [
+    {"image": pion, "position": (6, i), "joueur": 2} for i in range(8)
 ]
+
+# Position dans le plateau
+def is_outside(pos):
+    return all(0 <= coord < 8 for coord in pos)
 
 # Fonction pour dessiner le plateau
 def dessiner_plateau():
@@ -71,7 +74,6 @@ def dessiner_plateau():
             couleur = BLANC if (i + j) % 2 == 0 else NOIR
             pygame.draw.rect(fenetre, couleur, (j * TAILLE_CASE, i * TAILLE_CASE, TAILLE_CASE, TAILLE_CASE))
 
-
 # Fonction pour dessiner les pions
 def dessiner_pions():
     for pion in pions:
@@ -79,40 +81,59 @@ def dessiner_pions():
         y = pion["position"][0] * TAILLE_CASE + MARGE - 50 + (TAILLE_CASE - pion["image"].get_height()) // 2
         fenetre.blit(pion["image"], (x, y))
 
+# Fonction pour gérer le déplacement d'une pièce
+def deplacer_piece():
+    global pion_selectionne, joueur_actuel
+    x, y = pygame.mouse.get_pos()
+    x -= MARGE - 50
+    y -= MARGE - 50
+    nouvelle_position = (y // TAILLE_CASE, x // TAILLE_CASE)
+
+    if is_outside(nouvelle_position):
+        piece_existante = next((p for p in pions if p["position"] == nouvelle_position), None)
+        if piece_existante and piece_existante != pion_selectionne:
+            pions.remove(piece_existante)
+
+        pion_selectionne["position"] = nouvelle_position  # Update the position
+
+        return True  # Indique un déplacement réussi
+
+    return False  # Indique un déplacement échoué
+
+
 
 # Boucle principale du jeu
 en_cours = True
+joueur_actuel = 1  # Initialisez à 1 pour le premier joueur
 pion_selectionne = None
+
+clock = pygame.time.Clock()  # Ajoutez une horloge pour contrôler le taux de rafraîchissement
 
 while en_cours:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             en_cours = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            x, y = pygame.mouse.get_pos()
-            for pion in pions:
-                center_x = pion["position"][1] * TAILLE_CASE + MARGE - 50 + TAILLE_CASE / 2
-                center_y = pion["position"][0] * TAILLE_CASE + MARGE - 50 + TAILLE_CASE / 2
+            if event.button == 1:  # Vérifiez que le clic gauche est utilisé
+                for pion in pions:
+                    # Ajoutez une vérification pour s'assurer que le joueur sélectionne sa propre pièce
+                    if pion["joueur"] == joueur_actuel:
+                        center_x = pion["position"][1] * TAILLE_CASE + MARGE - 50 + TAILLE_CASE / 2
+                        center_y = pion["position"][0] * TAILLE_CASE + MARGE - 50 + TAILLE_CASE / 2
 
-                distance = ((x - center_x) ** 2 + (y - center_y) ** 2) ** 0.5
+                        distance = ((event.pos[0] - center_x) ** 2 + (event.pos[1] - center_y) ** 2) ** 0.5
 
-                if distance <= TAILLE_CASE / 2:
-                    pion_selectionne = pion
-                    break
+                        if distance <= TAILLE_CASE / 2:
+                            pion_selectionne = pion
+                            break
         elif event.type == pygame.MOUSEBUTTONUP:
-            if pion_selectionne:
-                 x, y = pygame.mouse.get_pos()
-                 x -= MARGE - 50
-                 y -= MARGE - 50
-                 nouvelle_position = (y // TAILLE_CASE, x // TAILLE_CASE)
+            if event.button == 1 and pion_selectionne:  # Vérifiez le bouton de la souris
+                deplacement_reussi = deplacer_piece()
 
-                 piece_existante = next((p for p in pions if p["position"] == nouvelle_position), None)
-
-                 if piece_existante and piece_existante !=  pion_selectionne:
-                      pions.remove(piece_existante)
-
-                 pion_selectionne["position"] = nouvelle_position  # Update the position
-                 pion_selectionne = None
+                if deplacement_reussi:
+                    # Si le déplacement a réussi, changez de joueur
+                    joueur_actuel = 3 - joueur_actuel  # Alterner entre les joueurs 1 et 2
+                pion_selectionne = None  # Réinitialisez la pièce sélectionnée après le déplacement
 
     # Effacer l'écran
     fenetre.fill(BLANC)
@@ -123,6 +144,9 @@ while en_cours:
 
     # Mettre à jour l'affichage
     pygame.display.flip()
+
+    # Contrôler le taux de rafraîchissement
+    clock.tick(30)  # 30 images par seconde (ajustez selon vos besoins)
 
 # Quitter Pygame
 pygame.quit()
