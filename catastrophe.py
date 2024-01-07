6e16d6bd3833c87186cbbe7b4573259e0977f169
Kirato <justkirato@gmail.com>
2024-01-07 18:23:30 +0100

    add catastrophe(s) specs

--- catastrophe.py
+++ catastrophe.py
@@ -1,9 +1,16 @@
 # catastrophe.py
 class Catastrophe:
     def __init__(self, name):
+        """
+        PRE : name est une chaîne de caractères représentant le nom de la catastrophe.
+        """
         self.name = name
 
     def trigger(self, board):
+        """
+        PRE : board est une instance de ChessBoard représentant l'échiquier.
+        POST : Affiche un message indiquant que l'événement de catastrophe a été déclenché.
+        """
         print(f"{self.name} event has been triggered!")
 
 
@@ -12,6 +19,11 @@ class Storm(Catastrophe):
         super().__init__("Storm")
 
     def trigger(self, board):
+        """
+        PRE : board est une instance de ChessBoard représentant l'échiquier.
+        POST : Affiche un message indiquant que l'événement de tempête a été déclenché.
+               Mélange les positions de toutes les pièces sur l'échiquier.
+        """
         super().trigger(board)
         board.shuffle_pieces()
 
@@ -21,5 +33,10 @@ class Transformation(Catastrophe):
         super().__init__("Transformation")
 
     def trigger(self, board):
+        """
+        PRE : board est une instance de ChessBoard représentant l'échiquier.
+        POST : Affiche un message indiquant que l'événement de transformation a été déclenché.
+               Transforme une pièce aléatoire en une reine.
+        """
         super().trigger(board)
         board.transform_random_piece_in_queen()
